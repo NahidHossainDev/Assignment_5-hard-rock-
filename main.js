54c33d952b918a747e7196d0185a689dfd9936e4
nahid9764 <nahidhossaain9764@gmail.com>
2020-08-18 17:44:44 +0600

    assignment complete

---
+++ main.js
@@ -0,0 +1,93 @@
+const apiUrl = 'https://api.lyrics.ovh/';
+
+function search() {
+    const userInput = document.querySelector('#input').value;
+    const usersQuery = apiUrl + "suggest/" + userInput;
+    console.log(usersQuery);
+    fetchElement(usersQuery).then(data => displayData(data));
+}
+async function fetchElement(api) {
+    const response = await fetch(api);
+    const data = await response.json();
+    return data;
+}
+
+let array = [];
+const displayData = (data) => {
+    console.log(data);
+    const display = document.getElementById('search-result');
+    display.innerHTML = "";
+    for (i = 0; i < 10; i++) {
+        const id = data.data[i].id;
+        const title = data.data[i].title;
+        const artistName = data.data[i].artist.name;
+        console.log("id =", id, 'title =', title, "artistName =", artistName);
+
+        const lyricsApi = 'https://api.lyrics.ovh/v1/' + artistName + "/" + title;
+        console.log(lyricsApi);
+        fetchingLyric(id, lyricsApi);
+
+       
+        // const findingLyric = array.filter(id => array.id == id);
+        // console.log(findingLyric);
+        // const detail = { id, title, artistName };
+        // array.push(detail);
+        display.innerHTML += `
+                <!-- single result -->
+                <div class="single-result row align-items-center my-3 p-3">
+                    <div class="col-md-9">
+                        <h3 class="lyrics-name">${title}</h3>
+                        <p class="author lead">Album by: <span>${artistName}</span>
+                          <img src="${data.data[i].artist.picture_small}" alt="Photos of Author">
+                        </p>
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button class="btn btn-success"  data-toggle="collapse" data-target="#a${id}" onclick="lyricShow('${id}')">Get Lyrics</button>
+                    </div>
+                </div>
+                <div class="result-lyric">
+                    <div class="single-lyrics text-center collapse" id="a${id}">
+                        <button class="btn go-back " data-toggle="collapse" data-target="#a${id}" >&lsaquo;</button>
+                        <h2 class="text-success mb-4">${title}</h2>
+                        <pre class="lyric text-white" id ="b${id}">
+    Wont go whistling like the wind blows,
+    Looking out my window,
+    Just to see the shine
+    Maybe you might call it crazy,
+    How Im acting lately,
+    </pre>
+                        <button class="btn go-back" data-toggle="collapse" data-target="#a${id}">&lsaquo;</button>
+                    </div>
+                </div>
+                <!-- ./ single result -->`;
+    }
+    console.log(array);
+}
+
+function fetchingLyric(id, lyricsApi) {
+    fetchElement(lyricsApi).then(data => array.push({id,data}));
+}
+function lyricShow(id) {
+    const lyricId = id;
+    let lyric;
+
+    // how to apply find method
+    for (i = 0; i < array.length; i++){
+        if (array[i].id == lyricId) {
+            lyric = array[i].data;
+            break;
+        }
+    }
+    console.log("result =", lyric);
+    
+    // if (lyric == lyric.error) {
+    //     console.log("error");
+    // } else {
+    if (lyric.lyrics == undefined) {
+        lyric.lyrics = "Ops...! No lyrics found...!!!"
+    }
+        console.log(lyric.lyrics)
+        const display = document.getElementById(`b${lyricId}`);
+        display.innerText = lyric.lyrics;
+    // }
+}
